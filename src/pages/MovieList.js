51cb45e9caa918defbc50e2b22c15e9260863572
GifftyCode <ulokangozi@yahoo.com>
2024-07-13 17:20:23 +0100

    base api fetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react';
 import { Card } from '../components';
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=d68cc6c72f0ddedc0897800df3b28c35'
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className='max-w-7xl mx-auto py-7'>
         <div className='flex justify-start flex-wrap'>
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -5,7 +5,7 @@ export const AllRoutes = () => {
   return (
     <div className='dark:bg-slate-800'>
       <Routes>
-        <Route path='/' element={<MovieList />} />
+        <Route path='' element={<MovieList />} />
         <Route path='movie/:id' element={<MovieDetail />} />
         <Route path='movies/popular' element={<MovieDetail />} />
         <Route path='movies/top' element={<MovieDetail />} />
