acbd3a6ccbe02766d823280b78aa00b6c696edd3
GifftyCode <ulokangozi@yahoo.com>
2024-07-13 14:57:45 +0100

    main page design

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -3,9 +3,9 @@ import { MovieDetail, MovieList, Search, PageNotFound } from '../pages';
 
 export const AllRoutes = () => {
   return (
-    <>
+    <div className='dark:bg-slate-800'>
       <Routes>
-        <Route path='' element={<MovieList />} />
+        <Route path='/' element={<MovieList />} />
         <Route path='movie/:id' element={<MovieDetail />} />
         <Route path='movies/popular' element={<MovieDetail />} />
         <Route path='movies/top' element={<MovieDetail />} />
@@ -13,6 +13,6 @@ export const AllRoutes = () => {
         <Route path='search' element={<Search />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
-    </>
+    </div>
   );
 };
