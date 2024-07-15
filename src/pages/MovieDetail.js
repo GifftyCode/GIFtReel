2af090f5079e09145e0dcaee2da00eb7f33b6fbf
GifftyCode <ulokangozi@yahoo.com>
2024-07-16 00:18:14 +0100

    movie detail api fetch

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,3 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
 export const MovieDetail = () => {
-  return <main>MovieDetail</main>;
+  const params = useParams();
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    async function fetchMovie() {
+      const response =
+        await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=d68cc6c72f0ddedc0897800df3b28c35
+`);
+      const result = await response.json();
+      setMovie(result);
+      console.log(result);
+    }
+    fetchMovie();
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <div>{setMovie.title}</div>
+        <div></div>
+      </section>
+    </main>
+  );
 };
